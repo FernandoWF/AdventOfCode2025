03bf09c2cc5fce7451a013af904c939fa35a1eb5
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2025-12-08 03:51:12 -0300

    Add day 8 solution

---
+++ Solutions/Day08/Solution.cs
@@ -0,0 +1,97 @@
+﻿namespace Solutions.Day08;
+
+internal sealed class Solution : ISolution
+{
+    static object ISolution.RunPart1(Input input) => RunPart1(input);
+
+    public static object RunPart1(Input input, int pairsToConnect = 1000)
+    {
+        ParseData(input, out var distanceByJunctionBoxPair, out var circuitByJunctionBox);
+
+        foreach (var (junctionBoxPair, distance) in distanceByJunctionBoxPair.OrderBy(pair => pair.Value).Take(pairsToConnect))
+        {
+            if (circuitByJunctionBox[junctionBoxPair.JunctionBox1] != circuitByJunctionBox[junctionBoxPair.JunctionBox2])
+            {
+                var newCircuit = circuitByJunctionBox[junctionBoxPair.JunctionBox1];
+                var oldCircuit = circuitByJunctionBox[junctionBoxPair.JunctionBox2];
+
+                foreach (var junctionBox in oldCircuit)
+                {
+                    newCircuit.Add(junctionBox);
+                    circuitByJunctionBox[junctionBox] = newCircuit;
+                }
+            }
+        }
+
+        return circuitByJunctionBox.Values
+            .Distinct()
+            .OrderByDescending(circuit => circuit.Count)
+            .Take(3)
+            .Aggregate(1, (result, circuit) => result * circuit.Count);
+    }
+
+    private static void ParseData(
+        Input input,
+        out Dictionary<(Position JunctionBox1, Position JunctionBox2), double> distanceByJunctionBoxPair,
+        out Dictionary<Position, HashSet<Position>> circuitByJunctionBox)
+    {
+        var junctionBoxes = input.Lines
+            .Select(line =>
+            {
+                var rawCoordinates = line.Split(',');
+                return new Position(int.Parse(rawCoordinates[0]), int.Parse(rawCoordinates[1]), int.Parse(rawCoordinates[2]));
+            })
+            .ToList();
+
+        distanceByJunctionBoxPair = [];
+        foreach (var junctionBox in junctionBoxes)
+        {
+            foreach (var otherJunctionBox in junctionBoxes)
+            {
+                if (junctionBox != otherJunctionBox
+                    && !distanceByJunctionBoxPair.ContainsKey((junctionBox, otherJunctionBox))
+                    && !distanceByJunctionBoxPair.ContainsKey((otherJunctionBox, junctionBox)))
+                {
+                    var distance = junctionBox.GetDistanceTo(otherJunctionBox);
+                    distanceByJunctionBoxPair.Add((junctionBox, otherJunctionBox), distance);
+                }
+            }
+        }
+
+        circuitByJunctionBox = junctionBoxes.ToDictionary(
+            junctionBox => junctionBox,
+            junctionBox => new HashSet<Position> { junctionBox });
+    }
+
+    public static object RunPart2(Input input)
+    {
+        ParseData(input, out var distanceByJunctionBoxPair, out var circuitByJunctionBox);
+        var circuits = circuitByJunctionBox.Values.ToHashSet();
+        (Position JunctionBox1, Position JunctionBox2) lastConnectedPair = default;
+
+        foreach (var (junctionBoxPair, distance) in distanceByJunctionBoxPair.OrderBy(pair => pair.Value))
+        {
+            if (circuitByJunctionBox[junctionBoxPair.JunctionBox1] != circuitByJunctionBox[junctionBoxPair.JunctionBox2])
+            {
+                var newCircuit = circuitByJunctionBox[junctionBoxPair.JunctionBox1];
+                var oldCircuit = circuitByJunctionBox[junctionBoxPair.JunctionBox2];
+
+                foreach (var junctionBox in oldCircuit)
+                {
+                    newCircuit.Add(junctionBox);
+                    circuitByJunctionBox[junctionBox] = newCircuit;
+                }
+
+                circuits.Remove(oldCircuit);
+                lastConnectedPair = junctionBoxPair;
+            }
+
+            if (circuits.Count == 1)
+            {
+                break;
+            }
+        }
+
+        return lastConnectedPair.JunctionBox1.X * lastConnectedPair.JunctionBox2.X;
+    }
+}
---
+++ Tests/Day08SolutionTests.cs
@@ -0,0 +1,42 @@
+﻿using Solutions;
+using Solutions.Day08;
+
+namespace Tests;
+
+public class Day08SolutionTests
+{
+    private static readonly Input ExampleInput = new("""
+        162,817,812
+        57,618,57
+        906,360,560
+        592,479,940
+        352,342,300
+        466,668,158
+        542,29,236
+        431,825,988
+        739,650,466
+        52,470,668
+        216,146,977
+        819,987,18
+        117,168,530
+        805,96,715
+        346,949,466
+        970,615,88
+        941,993,340
+        862,61,35
+        984,92,344
+        425,690,689
+        """);
+
+    [Fact]
+    public void RunPart1_ExampleInput_ReturnsCorrectOutput()
+    {
+        Assert.Equal(40, Solution.RunPart1(ExampleInput, 10));
+    }
+
+    [Fact]
+    public void RunPart2_ExampleInput_ReturnsCorrectOutput()
+    {
+        Assert.Equal(25272, Solution.RunPart2(ExampleInput));
+    }
+}
